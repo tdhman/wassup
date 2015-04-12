3dd1398556443b854282eb1510a8417b74d0489b
Hue Man <tdhman07@gmail.com>
2015-04-12 12:23:27 +0200

    fix fragement stack count

--- src/enst/infsi351/wassup/MainActivity.java
+++ src/enst/infsi351/wassup/MainActivity.java
@@ -240,7 +240,7 @@ public class MainActivity extends ActionBarActivity {
     
     @Override
     public void onBackPressed() {
-    	if (getFragmentManager().getBackStackEntryCount() > 0) {
+    	if (getFragmentManager().getBackStackEntryCount() > 1) {
             getFragmentManager().popBackStack();
         } else {
             super.onBackPressed();
